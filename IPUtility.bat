b522e51fb6f3073629f84a1838879105c23eecb4
Zach Anderson <Zach@speakinwebs.com>
2014-01-09 13:05:41 -0600

    Created a utility to configure IP settigs for windows computers.

---
+++ IPUtility.bat
@@ -0,0 +1,82 @@
+@echo off
+setlocal EnableDelayedExpansion
+echo -------------------------------------------------------------------------------
+echo                     FRC Team 4931 IP Configuration Utility
+echo ------------------------------------------------------------------------------
+::Test If script has Admin Priviledges/is elevated
+AT > NUL
+IF %ERRORLEVEL% EQU 0 (
+    goto START
+) ELSE (
+    ECHO Administrator privileges required. Please run as an administrator.
+    pause
+    EXIT /B 1
+)
+
+:START
+echo [1] Set IP address to interface with 2014 bridge.
+
+echo [2] Set IP address to interface with 2013 bridge.
+
+echo [3] Reset IP settings to automatic.
+SET /P choice=?
+cls
+echo -------------------------------------------------------------------------------
+echo                     FRC Team 4931 IP Configuration Utility
+echo ------------------------------------------------------------------------------
+IF %choice%== 1 goto NEW
+
+IF %choice%== 2 goto OLD
+
+IF %choice%== 3 goto REVERT
+
+
+
+:NEW
+echo Please enter assigned IP address.
+
+set /p address=10.49.31.
+IF%address%
+echo Setting IP Information...
+
+netsh interface ip set address "Wireless" static 10.49.31.%address% 255.0.0.0
+goto WAIT
+
+
+
+:OLD
+
+echo Please enter assigned IP address.
+
+set /p address=10.99.31.
+echo Setting IP Information...
+
+netsh interface ip set address "Wireless" static 10.49.31.%address% 255.0.0.0
+goto WAIT
+
+:REVERT
+
+echo Resetting IP configuration to automatic...
+
+netsh interface ip set address "Wireless" DHCP
+ipconfig /renew
+goto WAIT
+
+:WAIT
+GOTO LOOP
+
+:LOOP
+for /f %%i in ('ipconfig^|findstr IPv4') do set config=%%i
+IF [!config!]==[] GOTO LOOP
+
+
+
+:END
+
+cls
+echo -------------------------------------------------------------------------------
+echo                     FRC Team 4931 IP Configuration Utility
+echo -------------------------------------------------------------------------------
+echo Configuration successful.
+netsh interface ip show address "Wireless"
+pause
