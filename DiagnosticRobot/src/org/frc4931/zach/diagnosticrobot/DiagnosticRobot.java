9189945cce78d87f77ed611598f5fd7c25c075ad
Zach Anderson <Zach@speakinwebs.com>
2014-01-19 15:10:53 -0600

    Added a toggable strafe mode.

--- DiagnosticRobot/src/org/frc4931/zach/diagnosticrobot/DiagnosticRobot.java
+++ DiagnosticRobot/src/org/frc4931/zach/diagnosticrobot/DiagnosticRobot.java
@@ -1,7 +1,6 @@
 package org.frc4931.zach.diagnosticrobot;
 
 import org.frc4931.zach.diagnosticrobot.control.FlightStick;
-import org.frc4931.zach.diagnosticrobot.control.LogitechAttack;
 import org.frc4931.zach.diagnosticrobot.control.LogitechPro;
 import org.frc4931.zach.diagnosticrobot.drive.DriveTrain;
 import org.frc4931.zach.diagnosticrobot.io.AnalogInput;
@@ -16,11 +15,13 @@ public class DiagnosticRobot extends IterativeRobot{
 	public SpeedController leftMotor;
 	public SpeedController rightMotor;
 	public DriveTrain drive;
+	
+	public int buttonCooldown = 0;
+	public boolean strafe = false;
 	//@Override
 	public void robotInit(){
 		SmartDashboard.putData("Fader", new AnalogInput(1));
-		//SmartDashboard.putData("Motor", new AnalogOutput(1));
-		joystick = new LogitechAttack(1);
+		joystick = new LogitechPro(1);
 		SmartDashboard.putData("Joystick", joystick);
 		leftMotor = new Talon(1);
 		rightMotor = new Talon(2);
@@ -29,8 +30,25 @@ public class DiagnosticRobot extends IterativeRobot{
 	//@Override
 	public void teleopPeriodic(){
 		SmartDashboard.putData("Joystick", joystick);
-		double rawDriveSpeed = joystick.getPitch();
-		double rawTurnSpeed = joystick.getRoll()*-1;
+		buttonCooldown--;
+		buttonCooldown=Math.max(0, buttonCooldown);
+		if(joystick.getRawButton(2)&&buttonCooldown==0){
+			strafe=!strafe;
+			buttonCooldown = 10;
+		}
+		drive();
+	}
+	
+	private void drive(){
+		double rawDriveSpeed = 0;
+		double rawTurnSpeed = 0;
+		if(strafe){
+			rawDriveSpeed = joystick.getRoll();
+			rawTurnSpeed = joystick.getYaw()*-1;
+		}else if(!strafe){
+			rawDriveSpeed = joystick.getPitch();
+			rawTurnSpeed = joystick.getRoll()*-1;
+		}
 		double scaledDriveSpeed = rawDriveSpeed*joystick.getNormalThrottle();
 		double scaledTurnSpeed = rawTurnSpeed*joystick.getNormalThrottle();
 		drive.arcadeDrive(scaledDriveSpeed,scaledTurnSpeed);
