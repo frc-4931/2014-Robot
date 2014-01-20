2bd4dec0102803f1b6820c440b32283271b5dbea
Zach Anderson <Zach@speakinwebs.com>
2014-01-20 15:36:49 -0600

    Made performance and documentation improvements in DriveTrain.

--- DiagnosticRobot/src/org/frc4931/zach/diagnosticrobot/drive/DriveTrain.java
+++ DiagnosticRobot/src/org/frc4931/zach/diagnosticrobot/drive/DriveTrain.java
@@ -3,22 +3,36 @@ package org.frc4931.zach.diagnosticrobot.drive;
 import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.Sendable;
 import edu.wpi.first.wpilibj.tables.ITable;
-
+/**
+ * A class that represents the physical drive train.
+ * @author zach
+ */
 public class DriveTrain extends RobotDrive implements Sendable{
-	private Motor rightMotor;
-	private Motor leftMotor;
+	private final Motor rightMotor;
+	private final Motor leftMotor;
+	
+	private ITable table;
+	
+	/**
+	 * Creates a new drive train with a given left and right motor.
+	 * @param leftMotor the Motor object that represents the left motor.
+	 * @param rightMotor the Motor object that represents the right motor.
+	 */
 	public DriveTrain(Motor leftMotor, Motor rightMotor){
 		super(leftMotor.getController(), rightMotor.getController());
 		this.rightMotor=rightMotor;
 		this.leftMotor=leftMotor;
 	}
 	
+	/**Directly control left and right motor speed
+	 * @param leftSpeed the speed of the left motor in the range -1.0 to 1.0
+	 * @param rightSpeed the speed of the right motor in the range -1.0 to 1.0.
+	 */
 	public void directDrive(double leftSpeed, double rightSpeed){
 		tankDrive(leftSpeed, rightSpeed);
 	}
 	
 	/*SmartDashboard Information*/
-	private ITable table;
 	//@Override
 	public String getSmartDashboardType() {
 		return "Drive Train";
