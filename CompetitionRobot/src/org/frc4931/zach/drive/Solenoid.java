1c155adf3b98bd86c7bc2926a10f13df6188bfbe
Zach Anderson <Zach@speakinwebs.com>
2014-02-09 15:03:00 -0600

    Modified Solenoid to use the magnetic feedback switches.

--- CompetitionRobot/src/org/frc4931/zach/drive/Solenoid.java
+++ CompetitionRobot/src/org/frc4931/zach/drive/Solenoid.java
@@ -1,11 +1,21 @@
 package org.frc4931.zach.drive;
 
+import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 
 public class Solenoid {
 	private final DoubleSolenoid solenoid;
+	private final DigitalInput extendedSwitch;
+	private final DigitalInput retractedSwitch;
 	public Solenoid(int extendChannel, int retractChannel) {
 		solenoid = new DoubleSolenoid(extendChannel, retractChannel);
+		extendedSwitch = null;
+		retractedSwitch = null;
+	}
+	public Solenoid(int extendChannel, int retractChannel, int extendSwitch, int retractSwitch){
+		solenoid = new DoubleSolenoid(extendChannel, retractChannel);
+		extendedSwitch = new DigitalInput(extendSwitch);
+		retractedSwitch = new DigitalInput(retractSwitch);
 	}
 	public void extend(){
 		System.out.println("Solenoid Extended");
@@ -16,10 +26,18 @@ public class Solenoid {
 		solenoid.set(DoubleSolenoid.Value.kReverse);
 	}
 	public boolean isExtended(){
-		return (solenoid.get()==DoubleSolenoid.Value.kForward);
+		if(extendedSwitch == null){
+			return (solenoid.get()==DoubleSolenoid.Value.kForward);
+		}else{
+			return extendedSwitch.get();
+		}
 	}
 	public boolean isRetracted(){
-		return (solenoid.get()==DoubleSolenoid.Value.kReverse);
+		if(retractedSwitch == null){
+			return (solenoid.get()==DoubleSolenoid.Value.kReverse);
+		}else{
+			return retractedSwitch.get();
+		}
 	}
 
 }
