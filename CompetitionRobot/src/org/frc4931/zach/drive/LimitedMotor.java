4cef3d43610ea132bda38eb5b8740504376d5ac2
Zach Anderson <Zach@speakinwebs.com>
2014-08-28 23:13:56 -0500

    Improve function and documentation in LimitedMotor

--- CompetitionRobot/src/org/frc4931/zach/drive/LimitedMotor.java
+++ CompetitionRobot/src/org/frc4931/zach/drive/LimitedMotor.java
@@ -3,26 +3,69 @@ package org.frc4931.zach.drive;
 import edu.wpi.first.wpilibj.DigitalInput;
 
 public class LimitedMotor extends Motor{
-	public final DigitalInput highLimit;
-	public final DigitalInput lowLimit;
-	public LimitedMotor(int channel, int type, int lowLimit, int highLimit) {
+	private final DigitalInput highLimit;
+	private final DigitalInput lowLimit;
+	
+	/**
+	 * Constructs a new LimitedMotor with the on the using the specified channels and motor controller.
+	 * @param channel the pwm channel the motor controller is attached to. 
+	 * @param type the type of motor controller to use.
+	 * @param lowLimit the digital input channel of the low limit switch.
+	 * @param highLimit the digital input channel of the high limit switch.
+	 */
+	public LimitedMotor(int channel, Motor.SpeedControllerType type, int lowLimit, int highLimit) {
 		super(channel, type);
 		this.highLimit = new DigitalInput(highLimit);
 		this.lowLimit = new DigitalInput(lowLimit);
 	}
+	
+	/**
+	 * Tests if this motor has hit the high limit.
+	 * @return true if this motor has hit the high limit; false otherwise.
+	 */
 	public boolean isHigh(){
 		return !highLimit.get();
 	}
+	
+	/**
+	 * Tests if this motor has hit the high limit.
+	 * @return true if this motor has hit the low limit; false otherwise.
+	 */
 	public boolean isLow(){
 		return lowLimit.get();
 	}
-	//@Override
+	
+	/**
+	 * Move this motor towards the high limit at the specified speed.  This motor will not move
+	 * if it is already at the high limit.
+	 * @param speed the speed to turn the motor at.
+	 */
+	public void setHigh(double speed){
+		if(!isHigh()){
+			setSpeed(speed);
+		}else{
+			stop();
+		}
+	}
+	
+	/**
+	 * Move this motor towards the low limit at the specified speed.  This motor will not move
+	 * if it is already at the low limit.
+	 * @param speed the speed to turn the motor at.
+	 */
+	public void setLow(double speed){
+		if(!isLow()){
+			setSpeed(speed);
+		}else{
+			stop();
+		}	
+	}
+	
+	/**
+	 * Don't call this outside of LimitedMotor
+	 */
 	public void setSpeed(double speed){
-//		if(!isHigh()&&!isLow()){
-			controller.set(speed);
-//		}else{
-//			stop();
-//		}
+		controller.set(speed);
 	}
 
 }
